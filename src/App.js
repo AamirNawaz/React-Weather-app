41e33b56c8c4b2bb9f4f16121f77f4e36547b319
Aamir nawaz <vuprogramers@gmail.com>
2019-06-18 18:18:19 +0500

    https updated in api

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends React.Component {
     const countryName = event.target.elements.country.value;
 
     const api_call = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?q=${cityName},${countryName}&APPID=${API_KEY}`
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName},${countryName}&APPID=${API_KEY}`
     );
 
     const data = await api_call.json();
